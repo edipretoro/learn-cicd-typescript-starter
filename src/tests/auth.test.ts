151349f3b50f4e187a43289aa392e95f068d40ff
edipretoro <edipretoro@gmail.com>
2025-09-07 18:53:24 +0200

    Breaking the code to check the CI/CD pipeline

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -5,7 +5,7 @@ describe("getAPIKey", () => {
   test("should return null if the authorization header is not present", () => {
     const headers = {};
     const result = getAPIKey(headers);
-    expect(result).toBeNull();
+    expect(result).toBeTruthy();
   });
 
   test("should return null if the authorization header does not start with 'ApiKey'", () => {
